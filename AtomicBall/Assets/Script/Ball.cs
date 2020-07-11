2da350dd682f19999950d184c867eade089c46c1
Avilgor <35470345+Avilgor@users.noreply.github.com>
2020-07-11 02:22:35 +0200

    Update 2
    Added light and particles to ball
    Added goal and movable walls
    Added light to the goal
    Goal changes colour when ball touches it
    Ball and goal reposition in new round

--- AtomicBall/Assets/Script/Ball.cs
+++ AtomicBall/Assets/Script/Ball.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Ball : MonoBehaviour
 {
@@ -9,41 +10,63 @@ public class Ball : MonoBehaviour
     float count;
     [SerializeField] float angle;
     float rotationAngle;
+    bool locked;
+
     // Start is called before the first frame update
     void Start()
     {
         count = 0;
+        locked = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hitForward;
-        Physics.Raycast(transform.position, transform.forward, out hitForward, 3.0f, 1 << LayerMask.NameToLayer("Walls"));
-
-
-        if (hitForward.collider) Debug.DrawRay(transform.position, transform.forward * hitForward.distance, Color.red);
-        else Debug.DrawRay(transform.position, transform.forward * 3.0f, Color.green);
-
-
-        transform.Rotate(0, rotationAngle, 0);
-        if(!hitForward.collider)
+        if (Time.deltaTime > 0 && !locked)
         {
-            Vector3 pos = transform.position;
-            pos.z += transform.forward.z * speed;
-            pos.x += transform.forward.x * speed;
-            transform.position = pos;
-            ballMesh.transform.Rotate(0, 0, speed);
+            RaycastHit hitForward;
+            Physics.Raycast(transform.position, transform.forward, out hitForward, 3.0f, 1 << LayerMask.NameToLayer("Walls"));
+
+            if (Input.GetKeyDown(KeyCode.Space)) GameObject.Find("GameController").GetComponent<GameController>().GoalReached();
+
+            if (hitForward.collider) Debug.DrawRay(transform.position, transform.forward * hitForward.distance, Color.red);
+            else Debug.DrawRay(transform.position, transform.forward * 3.0f, Color.green);
+
+
+            transform.Rotate(0, rotationAngle, 0);
+            if (!hitForward.collider)
+            {
+                Vector3 pos = transform.position;
+                pos.z += transform.forward.z * speed;
+                pos.x += transform.forward.x * speed;
+                transform.position = pos;
+                ballMesh.transform.Rotate(0, 0, speed);
+            }
+
+            if (count < 1.0f)
+            {
+                count += Time.deltaTime;
+            }
+            else
+            {
+                count = 0;
+                rotationAngle = UnityEngine.Random.Range(-angle, angle);
+            }
         }
-        
-        if (count < 1.0f)
+    }
+
+    public void NextRound(float sp)
+    {
+        locked = false;
+        speed += sp;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Goal"))
         {
-            count += Time.deltaTime;           
-        }
-        else 
-        {
-            count = 0;
-            rotationAngle = UnityEngine.Random.Range(-angle, angle);         
+            GameObject.Find("GameController").GetComponent<GameController>().GoalReached();
+            locked = true;
         }
     }
 }
---
+++ AtomicBall/Assets/Script/GameController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] GameObject goal;
+    [SerializeField] GameObject goalGreen;
+    [SerializeField] GameObject ball;
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+    [SerializeField] float minY;
+    [SerializeField] float maxY;
+
+    [SerializeField] float speedIncrease;
+    int roundcounter;
+    bool goalReached;
+    float counter;
+
+    void Start()
+    {
+        Vector3 point = transform.position;
+        point.x = UnityEngine.Random.Range(minX,maxX);
+        point.z = UnityEngine.Random.Range(minY, maxY);
+        point.y = 1;
+        ball.transform.position = point;
+
+        point.x = UnityEngine.Random.Range(minX, maxX);
+        point.z = UnityEngine.Random.Range(minY, maxY);
+        point.y = 0.2f;
+        goal.transform.position = point;
+        roundcounter = 0;
+        goalReached = false;
+        counter = 0;
+    }
+
+    private void Update()
+    {
+        if (goalReached)
+        {
+            if (counter < 1.0f) counter += Time.deltaTime;
+            else Reposition();
+        }
+    }
+
+    public void GoalReached()
+    {
+        goalReached = true;
+        goal.SetActive(false);
+        goalGreen.transform.position = goal.transform.position;
+        goalGreen.SetActive(true);
+    }
+
+    private void Reposition()
+    {
+        Vector3 point = transform.position;
+        point.x = UnityEngine.Random.Range(minX, maxX);
+        point.z = UnityEngine.Random.Range(minY, maxY);
+        point.y = 1;
+        ball.transform.position = point;
+
+        point.x = UnityEngine.Random.Range(minX, maxX);
+        point.z = UnityEngine.Random.Range(minY, maxY);
+        point.y = 0.2f;
+        goal.transform.position = point;
+
+        if (roundcounter < 3)
+        { 
+            roundcounter++;
+            ball.GetComponent<Ball>().NextRound(0);
+        }
+        else
+        {
+            roundcounter = 0;
+            ball.GetComponent<Ball>().NextRound(speedIncrease);
+        }
+        goal.SetActive(true);
+        goalGreen.SetActive(false);
+        counter = 0;
+        goalReached = false;
+    }
+}
